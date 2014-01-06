077a5002fe14ea802dc70e87a35c6ffb4be062f6
James Luck <jamie.delucks@gmail.com>
2014-01-06 09:49:27 -0500

    Added start of maintenence script for new install, also imgur uploading script from internets

---
+++ dailyCleanup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Script to do all the stupid stuff that I do all the time for me
+
+mv ~/Screenshot* ~/screenshot/
+mv ~/*scrot* ~/screenshot/
+mv ~/downloads/*.torrent ~/documents/Dropbox/Oatmeal/Watched/
