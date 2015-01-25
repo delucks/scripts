542a416d358c12cdbc60a3029e0948dda383b344
delucks <jamie.delucks@gmail.com>
2015-01-25 18:13:27 -0500

    Updates to imgur downloading script, mainly trying to redo the functionality of imgur.sh more sanely

--- imgur.py
+++ imgur.py
@@ -1,11 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 import sys
+from urllib import urlretrieve
+import os
 
-def main(url):
+# this script is shitty hack level. don't expect it to be friendly or error-tolerant.
+
+# helper
+def getimage(img,outfolder):
+    # give it the http:// link as img, and folder to save to as outfolder
+    filename = img.split("/")[-1]
+    outpath = os.path.join(outfolder,filename)
+    urlretrieve(img, outpath)
+
+def main(url,outfolder):
   r = requests.get(url)
   soup = BeautifulSoup(r.text)
   for item in soup.find_all('img','thumb-title'):
-    print "https:" + item.get('data-src')[:-5] + ".jpg"
+    href = "https:" + item.get('data-src')[:-5] + ".jpg"
+    print href
+    getimage(href,outfolder)
 
-main(sys.argv[1])
+main(sys.argv[1],sys.argv[2])
