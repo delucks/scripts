e8c112ec6c2a106d17fcbfc336ba29b577f627ba
delucks <jamie.delucks@gmail.com>
2015-01-26 20:27:51 -0500

    Trimmed out a bunch of old files that are broken/not in use

--- dailyCleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script to do all the stupid stuff that I do all the time for me
-
-mv ~/Screenshot* ~/screenshot/
-mv ~/*scrot* ~/screenshot/
-mv ~/downloads/*.torrent ~/documents/Dropbox/Oatmeal/Watched/
-rmdir ~/Desktop/
-rmdir ~/Downloads/
