3f1915bc4be5f6ffc7534772b52b6846481936f8
delucks <jamie.delucks@gmail.com>
2015-02-02 11:54:44 -0500

    Updated color swapping script to account for new colors location

--- colorswap.sh
+++ colorswap.sh
@@ -1,9 +1,9 @@
-scheme=$(cd ~/dev/wp/colors/ && ls *.colors | dmenu)
+scheme=$(cd ~/dotfiles/colors/ && ls *.colors | dmenu)
 test -h ~/.colors && unlink ~/.colors
 test -h ~/.colorsX && unlink ~/.colorsX
 echo "$scheme"
-ln -s ~/dev/wp/colors/$scheme ~/.colors
+ln -s ~/dotfiles/colors/$scheme ~/.colors
 scheme+="X"
 echo "$scheme"
-ln -s ~/dev/wp/colors/$scheme ~/.colorsX
+ln -s ~/dotfiles/colors/$scheme ~/.colorsX
 xrdb -merge ~/.Xresources
