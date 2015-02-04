583fb8a56487d2dedd5348acb371b81ae5ef4218
delucks <jamie.delucks@gmail.com>
2015-02-04 12:18:36 -0500

    Added script to count characters of input

---
+++ charcount.py
@@ -0,0 +1,13 @@
+import sys
+
+count = {}
+with open(sys.argv[1]) as f:
+  for char in f.read():
+    if char not in '\n\t\ ':
+      if char in count:
+        count[char] += 1
+      else:
+        count[char] = 1
+
+for item in count.keys():
+  print "{key} {val}".format(key=item,val=count[item])
--- colorswap.sh
+++
@@ -1,9 +0,0 @@
-scheme=$(cd ~/dotfiles/colors/ && ls *.colors | dmenu)
-test -h ~/.colors && unlink ~/.colors
-test -h ~/.colorsX && unlink ~/.colorsX
-echo "$scheme"
-ln -s ~/dotfiles/colors/$scheme ~/.colors
-scheme+="X"
-echo "$scheme"
-ln -s ~/dotfiles/colors/$scheme ~/.colorsX
-xrdb -merge ~/.Xresources
