95e2aeda0ba9ff35eb3afa048bda3adc7f36333d
delucks <jamie.delucks@gmail.com>
2015-02-17 10:59:57 -0500

    Added script to try figlet fonts

---
+++ figletsample.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $# -le 1 ]]; then
+	echo "Usage: ./figletsample.sh 'Sample Text' filename.txt"
+	exit 1
+fi
+
+for font in /usr/share/figlet/fonts/*.flf;do
+	echo "$font" | tee -a "$2"
+	echo "$1" | figlet -f $font | tee -a "$2"
+	echo | tee -a "$2"
+	echo | tee -a "$2"
+	echo | tee -a "$2"
+done
