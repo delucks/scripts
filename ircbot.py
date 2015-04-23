441a6745213aeb6d0feea6521e5a865663f82089
delucks <jamie.delucks@gmail.com>
2015-04-22 23:11:45 -0400

    Slowly learning irc.bot

--- ircbot.py
+++ ircbot.py
@@ -5,12 +5,11 @@ from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
 class UtilBot(irc.bot.SingleServerIRCBot):
     def __init__(self, channel, nick, server, port):
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nick, nick)
-        #super(UtilBot,self).__init__(self, [(server, port)], nick, nick)
         self.channel = channel
         self.nick = nick
 
     def on_nicknameinuse(self, c, e):
-        c.nick(c.get_nickname() + "_")
+        c.nick(c.get_nickname() + '_')
     
     def on_welcome(self, c, e):
         c.join(self.channel)
@@ -19,34 +18,25 @@ class UtilBot(irc.bot.SingleServerIRCBot):
         self.do_command(e, e.arguments[0])
 
     def on_pubmsg(self, c, e):
-        a = e.arguments[0].split(":", 1)
-        if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(self.connection.get_nickname()):
-            self.handle_message(e, a[1].strip())
+        if (self.nick + ':' in e.arguments[0]):
+            a = e.arguments[0].split(':', 1)
+            if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(self.connection.get_nickname()):
+                self.handle_message(e, a[1].strip())
+        else if (e.arguments[0].startswith('!')):
+            self.handle_command(e.arguments[0])
+
+    def handle_command(self, e, cmd):
+        nick = e.source.nick
+        self.connection.notice(self.channel, '{n} tried to issue {c}'.format(n=nick,c=cmd))
 
     def handle_message(self, e, msg):
         nick = e.source.nick
-        c = self.connection
-        c.notice(self.channel, nick)
-        #if cmd == "smile":
-        #    c.notice(self.channel, smiles[0])
-        #elif cmd == "smile1":
-        #    c.notice(self.channel, smiles[1])
-        #elif cmd == "smile2":
-        #    c.notice(self.channel, smiles[2])
-        #elif cmd == "smile3":
-        #    c.notice(self.channel, smiles[3])
-        #elif cmd == "fail":
-        #    for failline in failwhale:
-        #        c.notice (self.channel, failline)
-        #elif cmd == "commands":
-        #    c.notice(nick, "smile, smile1, smile2, smile3, fail")
-        #else:
-        #    c.notice(self.channel, random.choice(smiles))
+        self.connection.notice(self.channel, '{n} is talking to me'.format(n=nick))
 
 def main():
     import sys
     if len(sys.argv) != 4:
-        print("Usage: testbot <server[:port]> <channel> <nickname>")
+        print('Usage: testbot <server[:port]> <channel> <nickname>')
         sys.exit(1)
 
     s = sys.argv[1].split(":", 1)
@@ -55,7 +45,7 @@ def main():
         try:
             port = int(s[1])
         except ValueError:
-            print("Error: Erroneous port.")
+            print('Error: Erroneous port.')
             sys.exit(1)
     else:
         port = 6667
@@ -65,5 +55,5 @@ def main():
     bot = UtilBot(channel, nickname, server, port)
     bot.start()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
