b1de9e927fbbd6724911fcb058f6fb38320405ef
delucks <jamie.delucks@gmail.com>
2015-04-23 13:08:25 -0400

    Actually opening the DAV site now

--- sakai_util.py
+++ sakai_util.py
@@ -2,6 +2,7 @@ import mechanize
 import cookielib
 from bs4 import BeautifulSoup
 import re
+from urlparse import urljoin
 
 '''
 This script interacts with the Sakai site
@@ -22,12 +23,16 @@ class SakaiUtil(object):
         self.br.set_handle_robots(False)
         self.br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
-    def get_classes(self):
+    def login(self):
         self.br.open(self.baseurl)
         self.br.select_form(nr=0)
         self.br.form['eid'] = self.eid
         self.br.form['pw'] = self.pw
         self.br.submit()
+
+    # call after login
+    def get_classes(self):
+        self.br.open(self.baseurl)
         soup = BeautifulSoup(self.br.response().read())
         class_uids = []
         for item in soup.find_all('a', role='menuitem'):
@@ -36,24 +41,39 @@ class SakaiUtil(object):
                 class_uids.append(link.split('/')[-1])
         return class_uids
 
-    def list_files(self, soup):
+    def list_files(self, url):
+        print url
+        self.br.open(url)
+        soup = BeautifulSoup(self.br.response().read())
         folders = set([])
-        for item in soup.find_all('li',class='folder'):
-            full_url = urljoin()
-            folders.add(item.get('href'))
+        files = set([])
+        for fo in soup.find_all('li', 'folder'):
+            folder_url = urljoin(url, fo.a.get('href'))
+            print 'Folder! {f}'.format(f=folder_url)
+            if folder_url != url:
+                folders.add(folder_url)
+        for fi in soup.find_all('li', 'file'):
+            file_url = urljoin(url, fi.a.get('href'))
+            print 'File! {f}'.format(f=file_url)
+            files.add(file_url)
+        for item in folders:
+            files = files.union(self.list_files(item))
+        return files
 
+    # will authenticate the dav page if necessary
     def get_files_for_class(self, class_uid):
         unformatted = re.sub('portal','dav/{uid}', self.baseurl)
         dav_baseurl = unformatted.format(uid=class_uid)
         #self.br.add_password(dav_baseurl, self.eid, self.pw)
         #self.br.open(dav_baseurl)
-        self.br.open('https://sakai.udel.edu/access/content/group/{uid}/'.format(uid=class_uid))
-        self.br.select_form(nr=0)
-        self.br.form['eid'] = self.eid
-        self.br.form['pw'] = self.pw
-        self.br.submit()
-        soup = BeautifulSoup(self.br.response().read())
-        return soup
+        url = 'https://sakai.udel.edu/access/content/group/{uid}/'.format(uid=class_uid)
+        self.br.open(url)
+        if (len([i for i in self.br.forms()]) == 1):
+            self.br.select_form(nr=0)
+            self.br.form['eid'] = self.eid
+            self.br.form['pw'] = self.pw
+            self.br.submit()
+        return self.list_files(url)
 
 def main():
     import argparse
@@ -62,7 +82,7 @@ def main():
     p.add_argument('-p', '--password', help='password')
     args = p.parse_args()
     su = SakaiUtil(eid=args.user, pw=args.password)
-    print su.get_files_for_class('020bfd02-8090-4d17-9109-aa98817feeea').prettify()
+    print su.get_files_for_class('020bfd02-8090-4d17-9109-aa98817feeea')
 
 if __name__=='__main__':
     main()
