1c268fef450729bdbb87164b08f1638525f75213
delucks <jamie.delucks@gmail.com>
2015-05-04 14:54:51 -0400

    Updated README. Removed unfriendly scraping module

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ A bunch of one-off scripts and ones vital to the operation of my custom desktop 
 
 ## Python
 
+* sakai_util.py: Work with sakai sites (bulk upload/download resources)
+* ircbot.py: Learning project to create a bot that automates tasks I do in IRC
 * Lsim.py: Encode/decode from the L-language used by Computational Theory (WIP)
 * x86.py: Dump instruction information from the official Intel reference
 * cdump.py: Dump pages of posts from craigslist so you can grep them
@@ -13,9 +15,13 @@ A bunch of one-off scripts and ones vital to the operation of my custom desktop 
 * roman: A python roman numeral translation engine. Wrote this to start learning Python.
 * res_sort.py: A python script to determine your wallpaper resolution and sort images based on it
 * redditlinkdump.py: Take a wild guess
+* term_escapes.py: Import me and get some new colors!
 
 ## Shell
 
+* keycheck.sh: Check to see if a given RSA public key exists on servers in your ~/.ssh/config
+* keyrotate.sh: Swap an old RSA key with a new key on all the servers in your ~/.ssh/config
+* current: Notification script that gives current information about my system
 * dmenu_ranger: Open a directory in ranger inside a new urxvt, cache it in case you're going to access it again
 * dmenu_man: Open a man page in a new urxvt
 * dmenu_ssh: Parses ~/.ssh/config for hosts, opens one in a new urxvt
--- linkvalidate.py
+++ linkvalidate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2
 # usage: python2 linkvalidate.py http://my.cool.website.com
-from ParserCommon import docurl
+import requests
 from bs4 import BeautifulSoup
 
 import argparse
@@ -8,9 +8,15 @@ from urlparse import urljoin
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument("page",type=str,help="First argument: the url to validate")
+  parser.add_argument('page',type=str,help='First argument: the url to validate')
+  parser.add_argument('-u', '--useragent', type=str, help='Alternate User-Agent field to use')
   args = parser.parse_args()
-  r = docurl(args.page)
+  if args.useragent:
+      headers = {'User-Agent': args.useragent}
+  else:
+    USER_AGENT = 'python link validation https://github.com/delucks/scripts/linkvalidate.py'
+    headers = {'User-Agent': USER_AGENT}
+  r = requests.get(args.page, headers=headers)
   soup = BeautifulSoup(r.text)
   # make the urls unique
   unique = {}
@@ -18,12 +24,12 @@ def main():
     unique[item.get('href')] = 1
   # check response code for each, print it
   for url in unique:
-    if (url.split(':')[0] == "mailto"):
+    if (url.split(':')[0] == 'mailto'):
       continue
-    if ((url.split(':')[0] != "http")or(url.split(':')[0] != "https")):
+    if ((url.split(':')[0] != 'http')or(url.split(':')[0] != 'https')):
         url = urljoin(args.page,url)
     req = docurl(url)
-    print "%i %s" % (req.status_code,url)
+    print '%i %s' % (req.status_code,url)
 
 if (__name__=='__main__'):
   main()
