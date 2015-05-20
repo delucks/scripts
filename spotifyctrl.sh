38ede513941fc88cdfea0f4ba3a4332bc215486d
delucks <jamie.delucks@gmail.com>
2015-05-20 11:42:54 -0400

    Added script to toggle my touchpad

--- spotifyctrl.sh
+++ spotifyctrl.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # a quick hack when I found out about the dbus interface
 TOGGLE="dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause"
 STOP="dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop"
