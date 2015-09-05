5302876463a17844987a297cd848820d0a55a375
delucks <jamie.delucks@gmail.com>
2015-09-04 18:07:11 -0700

    Updated wordcount.py, so it reads from stdin as well as files

--- wordcount.py
+++ wordcount.py
@@ -1,23 +1,11 @@
 #!/usr/bin/env python2
-import sys
+import fileinput
 
 count = {}
 
-if len(sys.argv) == 3:
-    input_file = sys.argv[2]
-elif len(sys.argv) == 2:
-    input_file = sys.argv[1]
-else:
-    print "Usage: python2 wordcount.py {-u} {filename}"
-    print "       The -u option will lower() each word in the file"
-    exit(1)
-
-with open(input_file) as f:
-    for token in f.read().split():
-        if sys.argv[1] == '-u':
-            word = token.lower()
-        else:
-            word = token
+for line in fileinput.input():
+    for token in line.split():
+        word = token.lower()
         if word in count:
             count[word] += 1
         else:
