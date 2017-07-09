a08ef5a6e1c94a596a0dc02b42bbe89575c6fd25
delucks <me@jamesluck.com>
2017-07-08 20:25:02 -0700

    Now works with arbitrary beets queries, added more documentation

--- fetch_beets_album.py
+++ fetch_beets_album.py
@@ -4,6 +4,14 @@ music collection using the web API, then compress it. If the (-z/--zip)
 argument is not provided, the resulting directory structure will be moved into
 the default music directory (~/Music). I *think* this only works on Linux but
 truly, who knows.
+
+Examples:
+    # Create a zip file of all music by Ricardo Villalobos
+    ./fetch_beets_album.py -z --artist 'Ricardo Villalobos'
+    # Create a zip file of all music released in 2014
+    ./fetch_beets_album.py -z -q 'year:2014'
+    # Use your beets API endpoint to fetch the album "Ok"
+    ./fetch_beets_album.py -a 'https://mycoolbeets.website:2337/' --album Ok
 '''
 import os
 import time
@@ -15,7 +23,7 @@ import urllib.request
 from tempfile import mkdtemp
 from urllib.parse import urljoin, quote
 
-__version__ = '0.0.7'
+__version__ = '0.1.0'
 __author__  = 'delucks'
 
 BEETS_WEB_ROOT = os.environ.get('BEETS_API')
@@ -37,23 +45,24 @@ def download_item(item_info, basepath, webroot):
     '''item information struct -> file on disk'''
     # Construct 04 - Song Title.filename
     # TODO add more zeros to the pad amount if there are > 99 tracks in this album
-    fn = '{num:02d} - {title}.{fmt}'.format(
+    filename = '{num:02d} - {title}.{fmt}'.format(
         num=int(item_info['track']),
         title=item_info['title'],
         fmt=item_info['format'].lower()
-    )
-    filename = fn.replace('/', '_')  # TODO find a better way to get around /
+    ).replace('/', '_')  # TODO find a better way to get around /
     file_endpoint = urljoin(webroot, 'item/{0}/file'.format(item_info['id']))
     # download the file
     print(':: Worker downloading {0} to {1}'.format(filename, basepath))
     return urllib.request.urlretrieve(file_endpoint, os.path.join(basepath, filename))
 
 def fetch_to_temp_folder(querystring, webroot=BEETS_WEB_ROOT):
-    resp = endpoint_json('/item/query/{0}'.format(querystring), webroot)
+    '''for a given beets Query, download all results into a temporary directory
+    with a directory structure given by their artist & album'''
     out_folder = mkdtemp(prefix='album_zip_')
     artists = set()
     # set up threads for concurrency on this slow network operation
     fabric = []
+    resp = endpoint_json('/item/query/{0}'.format(querystring), webroot)
     for item in resp['results']:
         item_info = endpoint_json('item/{0}'.format(item['id']), webroot)
         # Construct /tmp/foobar/Artist/Album/, and keep track of the base folder to zip it up later
@@ -69,11 +78,11 @@ def fetch_to_temp_folder(querystring, webroot=BEETS_WEB_ROOT):
     return out_folder, list(artists)
 
 def validate_api_available(endpoint):
-    if endpoint is None:
-        return False
     try:
-        resp = http_get(endpoint)
-        return b'<title>beets</title>' in resp
+        if endpoint is None:
+            return False
+        else:
+            return b'<title>beets</title>' in http_get(endpoint)
     except urllib.error.HTTPError:
         return False
 
@@ -82,22 +91,26 @@ def main():
     group = p.add_mutually_exclusive_group(required=True)
     group.add_argument('--artist', help='download all of an artist')
     group.add_argument('--album', help='download an entire album')
+    group.add_argument('-q', '--query', help='download all results of this beets query')
     p.add_argument('-z', '--zip', action='store_true', help='zip up the files')
-    p.add_argument('--api', help='the root path of the beets web API')
+    p.add_argument('-a', '--api', help='the root path of the beets web API')
     args = p.parse_args()
     webroot = args.api or BEETS_WEB_ROOT
     if not validate_api_available(webroot):
         print('[FATAL] API cannot be contacted! Check your --api argument or set the environment variable BEETS_API')
         os.sys.exit(1)
     # Assemble the beets query and download it to a temp folder
-    qs = 'artist:' + args.artist if args.artist else 'album:' + args.album
+    qs = args.query if args.query else 'artist:' + args.artist if args.artist else 'album:' + args.album
     print('Starting download of query {0}'.format(qs))
     basename, dirs = fetch_to_temp_folder(qs, webroot)
     # post-process the created files
     print('Downloaded successfully.')
     if args.zip:
         # make a zip file out of the directories taken down
-        zip_name = os.path.expanduser('~/{0}_{1}'.format(args.artist or args.album, int(time.time())))
+        zip_name = os.path.expanduser('~/{0}_{1}'.format(
+            args.query or args.artist or args.album,
+            int(time.time())
+        ))
         shutil.make_archive(zip_name, format='zip', root_dir=basename, base_dir='.')
         print('Music should be in {0}'.format(zip_name + '.zip'))
         shutil.rmtree(basename)
